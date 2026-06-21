d98c8a33459bed6753c5e4b0ed4db6c5a296f347
DIVYA KONDAMEEDI <divyakondameedi2@gmail.com>
2026-06-21 15:23:28 +0530

    Add Amma Muddha Restaurant Website (Task 3)

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import WhatsAppButton from "./components/WhatsAppButton";
+
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+import Testimonials from "./pages/Testimonials";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+      <WhatsAppButton />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/WhatsAppButton.jsx
@@ -0,0 +1,12 @@
+export default function WhatsAppButton() {
+  return (
+    <a
+      href="https://wa.me/919876543210"
+      target="_blank"
+      rel="noreferrer"
+      className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-xl text-3xl z-50"
+    >
+      💬
+    </a>
+  );
+}
